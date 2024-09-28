47793783a3375e1632620d42171e20100b54b09b
Leo Jia <grassblogstu@gmail.com>
2024-09-28 22:48:49 +0800

    feat: Add config check function

--- src/internal/config/common.rs
+++ src/internal/config/common.rs
@@ -1,29 +1,47 @@
 use super::structure::Config;
+use log::warn;
 use std::fs::File;
+use std::io;
 use std::io::Read;
 use std::path::Path;
 
+const CONFIG_VERSION: u8 = 1;
+
 impl Config {
-    pub fn new() -> Self {
+    pub fn new(host: String, port: u32) -> Self {
         Config {
-            version: 1,
-            host: Some("127.0.0.1".into()),
-            port: Some(7817),
+            version: CONFIG_VERSION,
+            host: Some(host),
+            port: Some(port),
         }
     }
 
-    pub fn from_file() -> Self {
-        let file: &Path = Path::new("./config.toml");
+    pub fn from_file(path: &str) -> io::Result<Self> {
+        let file: &Path = Path::new(path);
         let file = File::open(file);
         let mut file = match file {
             Ok(f) => f,
-            Err(err) => panic!("Unable to open config file: {}", err),
+            Err(err) => return Err(err),
         };
         let mut config: String = String::new();
-        if let Err(err) = file.read_to_string(&mut config) {
-            panic!("{}", err);
+        file.read_to_string(&mut config)?;
+        Ok(config.into())
+    }
+
+    pub fn check(&mut self) {
+        // Check if host is set, if not then set it to "127.0.0.1"
+        if self.host.is_none() {
+            self.host = Some("127.0.0.1".into());
         }
-        config.into()
+        // Check if port is set, if not then set it to 7817
+        if self.port.is_none() {
+            self.port = Some(7817);
+        } else if let Some(port) = self.port {
+            if !(1024..=65535).contains(&port) {
+                self.port = Some(7817);
+                warn!("Port number {} is out of range, setting port number to default value (7817)", port);
+            }
+        }
     }
 }
 
@@ -48,7 +66,7 @@ impl From<Config> for String {
 impl Default for Config {
     fn default() -> Self {
         Config {
-            version: 1,
+            version: CONFIG_VERSION,
             host: Some("127.0.0.1".into()),
             port: Some(7817),
         }
