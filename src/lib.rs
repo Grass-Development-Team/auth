dad2c3559dd384d4c3720bf131e9a2ac6e42e587
Leo Jia <grassblogstu@gmail.com>
2024-09-28 22:49:01 +0800

    feat: Read config

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 mod routers;
 mod internal;
 
+use crate::internal::config::structure::Config;
 use crate::routers::get_router;
 use axum::Router;
+use log::warn;
 use tokio::net::TcpListener;
 
 pub async fn run() {
+    let mut config = Config::from_file("config.toml")
+        .unwrap_or_else(|_| {
+            warn!("Cannot load config file. Use default config instead.");
+            Config::default()
+        });
+
+    config.check();
+
     let app = Router::new();
-    let listener = TcpListener::bind("127.0.0.1:9000")
+    let listener = TcpListener::bind(format!("{}:{}", config.host.unwrap(), config.port.unwrap()))
         .await.unwrap();
     axum::serve(listener, get_router(app))
         .await.unwrap();
