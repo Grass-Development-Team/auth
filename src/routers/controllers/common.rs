e83198cbaa3bbb44853d4d944ad6debcaac5ee81
Leo Jia <grassblogstu@gmail.com>
2024-10-13 18:24:01 +0800

    refactor: Update serialize code

--- src/routers/controllers/common.rs
+++ src/routers/controllers/common.rs
@@ -3,7 +3,7 @@ use axum::http::StatusCode;
 use axum::Json;
 
 pub async fn not_found() -> (StatusCode, Json<Response>) {
-    (StatusCode::NOT_FOUND, Json(Response::new_error(ResponseCode::NotFound.into(), "Not Found".into())))
+    (StatusCode::NOT_FOUND, Json(Response::new_error(ResponseCode::NotFound.into(), ResponseCode::NotFound.into())))
 }
 
 pub async fn ping() -> Json<Response> {
