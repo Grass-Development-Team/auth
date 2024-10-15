f06b5523e0e977cb6352b7b7782192c148fe41a3
yuanzui-cf <grassblogstu@gmail.com>
2024-10-15 23:58:58 +0800

    fix: Password hash generate util

--- src/internal/utils/password.rs
+++ src/internal/utils/password.rs
@@ -3,7 +3,7 @@ use sha2::Digest;
 pub fn generate(password: String, salt: String) -> String {
     let password_with_salt = format!("{}{}", password, salt);
     let hash = sha2::Sha256::digest(password_with_salt);
-    base16ct::lower::encode_string(&hash);
+    base16ct::lower::encode_string(&hash)
 }
 
 pub fn check(password: String, salt: String, hash_to_check: String) -> bool {
