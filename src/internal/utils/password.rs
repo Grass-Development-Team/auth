00d9bdf6315128b7be1f77ac52b75ff0607b6962
yuanzui-cf <grassblogstu@gmail.com>
2024-10-15 23:52:23 +0800

    feat: Add password hash generate utils

--- src/internal/utils/password.rs
+++ src/internal/utils/password.rs
@@ -1,12 +1,12 @@
 use sha2::Digest;
 
 pub fn generate(password: String, salt: String) -> String {
-    todo!()
+    let password_with_salt = format!("{}{}", password, salt);
+    let hash = sha2::Sha256::digest(password_with_salt);
+    base16ct::lower::encode_string(&hash);
 }
 
 pub fn check(password: String, salt: String, hash_to_check: String) -> bool {
-    let password_with_salt = format!("{}{}", password, salt);
-    let hash = sha2::Sha256::digest(password_with_salt);
-    let hash = base16ct::lower::encode_string(&hash);
+    let hash = generate(password, salt);
     hash == hash_to_check
 }
