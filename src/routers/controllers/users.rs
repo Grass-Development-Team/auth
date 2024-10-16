088dc777680b73dbda034845fc2fb0d1ae528774
yuanzui-cf <grassblogstu@gmail.com>
2024-10-17 01:50:39 +0800

    feat: Register controller

--- src/routers/controllers/users.rs
+++ src/routers/controllers/users.rs
@@ -1,12 +1,14 @@
 use crate::internal::serializer::common::Response;
-use axum::Json;
+use crate::services::users::{LoginService, RegisterService};
+use crate::state::AppState;
+use axum::{Json, Extension};
 
 /// User register
-pub async fn register() -> Json<Response> {
-    todo!("Register controller");
+pub async fn register(Json(req): Json<RegisterService>, Extension(state): Extension<AppState>) -> Json<Response<&str>> {
+    Json(req.register(&state.db))
 }
 
 /// User login
-pub async fn login() -> Json<Response> {
+pub async fn login(Json(req): Json<LoginService>, Extension(state): Extension<AppState>) -> Json<Response> {
     todo!("Login controller");
 }
