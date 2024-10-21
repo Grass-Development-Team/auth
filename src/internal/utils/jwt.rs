4487da2aa582d53a75395a69fb915af57849bb73
Leo Jia <grassblogstu@gmail.com>
2024-10-21 23:23:24 +0800

    feat: Add generate_claim & validate jwt utils

--- src/internal/utils/jwt.rs
+++ src/internal/utils/jwt.rs
@@ -1,3 +1,4 @@
+use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 
@@ -20,10 +21,24 @@ pub fn generate(claim: Claim, secret: &str) -> jsonwebtoken::errors::Result<Stri
     encode(&Header::default(), &claim, &EncodingKey::from_secret(secret.as_ref()))
 }
 
+pub fn generate_claim(iss: String, sub: String, uid: u32, sid: String) -> Claim {
+    Claim {
+        iss,
+        sub,
+        exp: (Utc::now() + Duration::days(7)).timestamp() as usize,
+        uid,
+        sid,
+    }
+}
+
 pub fn unwrap(jwt: &str, secret: &str) -> jsonwebtoken::errors::Result<Claim> {
     let claim = match decode::<Claim>(jwt, &DecodingKey::from_secret(secret.as_ref()), &Validation::default()) {
         Ok(claim) => claim,
         Err(err) => return Err(err),
     };
     Ok(claim.claims)
+}
+
+pub fn validate(claim: &Claim) -> bool {
+    claim.exp < (Utc::now().timestamp() as usize)
 }
