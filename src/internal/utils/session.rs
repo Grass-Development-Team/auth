31e88d4a32b463605aa8df0971f1a247aceb6d41
Leo Jia <grassblogstu@gmail.com>
2024-10-22 23:00:43 +0800

    refactor: Change uid from u32 to i32

--- src/internal/utils/session.rs
+++ src/internal/utils/session.rs
@@ -3,11 +3,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Session {
-    pub uid: u32,
+    pub uid: i32,
     pub exp: usize,
 }
 
-pub fn generate(uid: u32) -> Session {
+pub fn generate(uid: i32) -> Session {
     Session {
         uid,
         exp: (Utc::now() + Duration::days(7)).timestamp() as usize,
