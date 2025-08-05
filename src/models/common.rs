02c76473350dff4606cb32d3aec8c8b0694f35e0
yuanzui-cf <grassblogstu@gmail.com>
2025-08-05 22:48:47 +0800

    feat: Implement Display and Error traits for ModelError

--- src/models/common.rs
+++ src/models/common.rs
@@ -1,7 +1,27 @@
+use std::error::Error;
+
 use sea_orm::DbErr;
 
 #[derive(Debug)]
 pub enum ModelError {
     DBError(DbErr),
     Empty,
+}
+
+impl std::fmt::Display for ModelError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ModelError::DBError(err) => write!(f, "Database error: {}", err),
+            ModelError::Empty => write!(f, "No entity found or empty result"),
+        }
+    }
+}
+
+impl std::error::Error for ModelError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ModelError::DBError(err) => Some(err),
+            ModelError::Empty => None,
+        }
+    }
 }
