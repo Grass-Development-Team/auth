b86565e7b8b6b29ac0a1d27ef6a92593b366a47c
yuanzui-cf <im.leojia@gmail.com>
2026-03-12 12:20:26 +0800

    refactor(config): Add MailSecure enum and replace tls with secure for mail configuration

--- src/internal/config/structure/mail.rs
+++ src/internal/config/structure/mail.rs
@@ -1,15 +1,41 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, Eq)]
+pub enum MailSecure {
+    #[serde(alias = "tls", alias = "starttls", alias = "STARTTLS")]
+    TLS,
+    #[serde(alias = "ssl", alias = "smtps", alias = "implicit_tls")]
+    SSL,
+    #[serde(alias = "none", alias = "plain")]
+    None,
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Mail {
     pub host:     String,
-    #[serde(default = "default_port")]
-    pub port:     u16,
+    pub port:     Option<u16>,
     pub username: String,
     pub password: String,
-    pub tls:      bool,
+    #[serde(default = "default_secure")]
+    pub secure:   MailSecure,
 }
 
-fn default_port() -> u16 {
-    587
+fn default_secure() -> MailSecure {
+    MailSecure::TLS
+}
+
+impl MailSecure {
+    pub fn default_port(self) -> u16 {
+        match self {
+            MailSecure::TLS => 587,
+            MailSecure::SSL => 465,
+            MailSecure::None => 25,
+        }
+    }
+}
+
+impl Mail {
+    pub fn port(&self) -> u16 {
+        self.port.unwrap_or_else(|| self.secure.default_port())
+    }
 }
